58abf5d8e78d84f55fbced6cd847e02f400ab2d6
岩本一真 <iwamotokazuma@iwamotokazumanoMacBook-Pro.local>
2020-06-28 09:58:51 +0900

    slick

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,11 @@
 //= require_tree .
 //= require jquery
 //= require bootstrap-sprockets
+
+$(function() {
+    $('.a').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+    });
+});
